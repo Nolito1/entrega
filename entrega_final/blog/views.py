4c8fab847d98ce36e85c9b955bcbd6eee2f1e54a
Esteban <Esteban@DESKTOP-V66C70P>
2022-06-15 10:08:08 -0300

    Agregado no borrar si no pertenece al usuario

--- entrega_final/blog/views.py
+++ entrega_final/blog/views.py
@@ -1,13 +1,16 @@
+
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from blog.models import BlogModel
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import PermissionDenied
+
 
 
 
@@ -27,7 +30,12 @@ class BlogCreate(LoginRequiredMixin, CreateView):
 
     model = BlogModel
     success_url = reverse_lazy("blog_list")
-    fields = ["titulo", "sub_titulo", "cuerpo", "autor"]
+    fields = ["titulo", "sub_titulo", "cuerpo"]
+
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+
 
 
 class BlogUpdate(LoginRequiredMixin, UpdateView):
@@ -37,15 +45,20 @@ class BlogUpdate(LoginRequiredMixin, UpdateView):
     fields = ["titulo", "sub_titulo", "cuerpo", "autor"]
 
 
-class BlogDelete(LoginRequiredMixin, DeleteView):
+class BlogDelete(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
 
     model = BlogModel
     success_url = reverse_lazy("blog_list")
+
+    def test_func(self):
+     exist = BlogModel.objects.filter(autor=self.request.user.id, id=self.kwargs['pk'])
+     return True if exist else False
 
 
 class BlogLogin(LoginView):
     template_name = 'blog/blog_login.html'
     next_page = reverse_lazy("blog_list")
+   
 
 
 class BlogLogout(LogoutView):
@@ -58,3 +71,4 @@ class SignUpView(SuccessMessageMixin, CreateView):
   success_url = reverse_lazy('blog_login')
   form_class = UserCreationForm
   success_message = "Your profile was created successfully"
+
